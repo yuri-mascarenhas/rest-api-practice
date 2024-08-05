5bca48977a7cda83ad78f502edbff95916c971f2
yurism <mascarenhasyuri@hotmail.com>
2024-08-04 22:17:27 -0300

    [Add] method override to app

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { fileURLToPath } from "url";
 import * as path from "path";
 import { v4 as uuid } from "uuid";
+import methodOverride from "method-override";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -29,6 +30,7 @@ const mockComments = [
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
